34029088e6881596380cfe8542ebc55ece59c986
Kesomannen <bobbo.nidor@gmail.com>
2023-12-01 21:44:16 +0100

    fix

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,14 +1,20 @@
 ﻿using Lilypad;
 using Lilypad.Helpers;
+using Lilypad.Text;
 
 const string outputPath = @"C:\Users\bobbo\AppData\Roaming\PrismLauncher\instances\Default 1.20.1\.minecraft\saves\Testing\datapacks\lilypad-test";
 
 var datapack = new Datapack("test");
 
 datapack.Functions.Create(f => {
-    f.Execute().As("@a").At("@s").Run(f => {
-        f.RaycastForward(8, Selector.Entites, f => f.Kill());
-    });
+    var objective = datapack.Objectives.Create();
+    var variable = new ScoreVariable("test", objective);
+
+    f.Increment(variable);
+    f.Operation(variable, "-=", 2);
+    f.Operation(variable, "/=", 5);
+    
+    f.Tellraw("@a", $"Result: <{variable}>");
 }).SetTick();
 
 datapack.Transpile(outputPath);
